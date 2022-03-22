6ddb2c351941a520262a5d6c06d40f5ec6b0e19f
Aaron Roethe <roethe.aaron@outlook.com>
2022-03-22 11:25:19 -0700

    project init

---
+++ run.bat
@@ -0,0 +1,15 @@
+set venv=ringcentral_api
+:: -------------------------------------------------------------------------
+:: Open anaconda & activate env
+:: -------------------------------------------------------------------------
+call %USERPROFILE%\Anaconda3\Scripts\activate %USERPROFILE%\Anaconda3 
+call activate %venv%
+:: -------------------------------------------------------------------------
+:: Change directory to relative path
+:: -------------------------------------------------------------------------
+cd %~dp0
+:: -------------------------------------------------------------------------
+:: Run script at this location
+:: -------------------------------------------------------------------------
+call %USERPROFILE%/Anaconda3/envs/%venv%/python.exe "%~dp0\src\main.py"
+PAUSE
---
+++ setup.bat
@@ -0,0 +1,7 @@
+set venv=ringcentral_api
+call %USERPROFILE%\Anaconda3\Scripts\activate %USERPROFILE%\Anaconda3 
+call activate base
+
+call conda env create --file environment.yml
+call activate %venv%
+PAUSE

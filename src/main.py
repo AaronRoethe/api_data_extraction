8c44b9db095cebcb2318bb8069c3521d38e7fcf8
Aaron Roethe <roethe.aaron@outlook.com>
2022-03-23 11:51:13 -0700

    implement logger

--- src/main.py
+++ src/main.py
@@ -13,44 +13,50 @@ Updated on Fri Mar 18
 from datetime import datetime, date
 
 from pipeline.config import api_keys
-from pipeline.utils import last_business_day
-from pipeline.extract import get_access_token, request_report
+from pipeline.extract import api
+from pipeline.utils import last_business_day, class_inputs, save_df_info
 from pipeline.transform import decode_encode, string_to_df, clean_df
 from pipeline.load import sql_engine, load_MSSQL
+from pipeline.log import log_everthing
 
 def main():
     ### log start time
     starttime = datetime.now()
-    print(starttime)
-    # report_500 = api(api_key,secret,reportId,startDate,endDate)
 
     ### inputs
     api_key, secret = api_keys()
     startDate       = last_business_day(date.today()).isoformat()
     endDate         = date.today().isoformat()
     reportId        = '500'
-    server          = 'EUS1PCFSNAPDB01'
+    # server          = 'EUS1PCFSNAPDB01'
+    server          = 'EUS1QCFSNAPDB01'
     database        = 'DWWorking'
     table           = 'api_nic_agentbyday'
-    
-    ### pipeline
-    ## Generate Access token
-    accessToken = get_access_token(api_key, secret)
-    ## Post request
-    #generate info as base64
-    #Define parameters for request
-    api_requested_data = request_report(
-        accessToken, startDate, endDate, reportId=reportId)
-    ## clean requested data
-    str_data    = decode_encode(api_requested_data)
-    df          = string_to_df(str_data)
-    load        = clean_df(df)
-    print(load)
-    ## load into server
-    # engine = sql_engine(server, database)
-    # load_MSSQL(engine, load, table_name=table)
 
-    print("Time to Complete:",datetime.now() - starttime)
+    ### call api class    
+    report_500 = api(api_key, secret, reportId, startDate, endDate)
+    ### collect & log inputs
+    log_everthing("INPUTS",class_inputs(report_500))
+
+    ### api requested data
+    response = report_500.request_report()
+    log_everthing("STATUS_CODE", (response.status_code, response.reason))
+
+    ## clean requested data
+    str_data = decode_encode(response.json())
+    log_everthing("READABLE", str_data.readable())
+
+    df = string_to_df(str_data)
+    # log_everthing("DF_INFO: {save_df_info(df)}")
+
+    load = clean_df(df)
+    # log_everthing(f"CLEAN DF_INFO: {save_df_info(load)}")
+    print(load)
+    # load into server
+    if input("Do you want to insert into SQL(y/n): ") == 'y':
+        engine = sql_engine(server, database)
+        load_MSSQL(engine, load, table_name=table)
+    log_everthing("COMPLETED", datetime.now() - starttime)
 
 if __name__ == "__main__":
     main()

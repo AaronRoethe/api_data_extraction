ec39a2b5bcc32d088fd5db33784976d0520e575a
Aaron Roethe <roethe.aaron@outlook.com>
2022-03-23 11:48:33 -0700

    convert func to api class

--- src/pipeline/extract.py
+++ src/pipeline/extract.py
@@ -1,47 +1,53 @@
 from dataclasses import dataclass
-from urllib import response
 import requests, json
 from requests.structures import CaseInsensitiveDict
 
-def get_access_token(api_key:str, secret:str):
-    url = "https://na1.nice-incontact.com/authentication/v1/token/access-key"
+@dataclass
+class api():
+    key:str
+    secret:str
+    reportId:str
+    start:str
+    end:str
+    fileName:str        = 'test_3.csv'
+    saveAsFile:str      = 'FALSE'
+    includeHeaders:str  = 'TRUE'
+    baseurl:str         = 'https://api-c52.nice-incontact.com/incontactapi'
+    download_path:str   = 'services/v23.0/report-jobs/datadownload'
+    
+    def access_token(self):
+        url = "https://na1.nice-incontact.com/authentication/v1/token/access-key"
+        headers = {'Content-Type': 'application/json'}
+        payload = f"""{{
+                    "accessKeyId":"{self.key}",
+                    "accessKeySecret":"{self.secret}"
+                    }}"""
+        response = requests.request("POST", url, headers=headers, data=payload)
+        data = json.loads(response.text)
+        return data["access_token"]
+    
+    def url(self):
+        return f"{self.baseurl}/{self.download_path}/{self.reportId}?startDate={self.start}&endDate={self.end}&saveAsFile={self.saveAsFile}&includeHeaders={self.includeHeaders}" 
 
-    payload = f"""{{
-                "accessKeyId":"{api_key}",
-                "accessKeySecret":"{secret}"
-                }}"""
+    def headers(self):
+        header_param = CaseInsensitiveDict()
+        header_param['Authorization'] = f"bearer {self.access_token()}"
+        header_param['Content-Type']  = 'application/x-www-form-urlencoded'
+        header_param['Accept']        = 'application/json, text/javascript, */*'
+        return header_param
 
-    headers = {
-    'Content-Type': 'application/json'
-    }
-    #Generate response
-    response = requests.request("POST", url, headers=headers, data = payload)
-    data = json.loads(response.text) #load to json 
-    return data["access_token"]
+    def payload(self):
+            return {
+            'reportId'  : self.reportId,
+            'fileName'  : self.fileName,
+            'startDate' : self.start,
+            'endDate'   : self.end,
+            'saveAsFile': self.saveAsFile,
+            'includeHeaders' : self.includeHeaders
+            }
 
-def request_report(accessToken, start, end, reportId, fileName='test_3.csv',saveAsFile='false',includeHeaders='true'):
-    BASEURL = 'https://api-c52.nice-incontact.com/incontactapi'
-    # Define parameters in payload
-    payload={
-    'reportId'  : reportId,
-    'fileName'  : fileName,
-    'startDate' : start,
-    'endDate'   : end,
-    'saveAsFile': saveAsFile,
-    'includeHeaders' : includeHeaders
-    }
-
-    #add required headers
-    header_param = CaseInsensitiveDict()
-    header_param['Authorization'] = f"bearer {accessToken}"
-    header_param['Content-Type']  = 'application/x-www-form-urlencoded'
-    header_param['Accept']        = 'application/json, text/javascript, */*'
-
-    # Make http post request
-    download_path = 'services/v23.0/report-jobs/datadownload'
-    url = f"{BASEURL}/{download_path}/{reportId}?startDate={start}&endDate={end}&saveAsFile={saveAsFile}&includeHeaders={includeHeaders}" 
-    response = requests.post(url, headers=header_param, data=payload)
-    #Check response codes
-    print(response.status_code, response.reason)
-    # #Cast response to json
-    return response.json()
+    def request_report(self):
+        # Make http post request
+        response = requests.post(self.url(), headers=self.headers(), data=self.payload())
+        return response
+        
